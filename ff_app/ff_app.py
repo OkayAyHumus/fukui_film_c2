1e3be51a2ae2346c637308f512e452d797fc5c94
OkayAyHumus <97745736+OkayAyHumus@users.noreply.github.com>
2025-06-04 23:36:31 +0900

    Update ff_app.py

--- ff_app/ff_app.py
+++ ff_app/ff_app.py
@@ -12,7 +12,8 @@ import sys
 from io import BytesIO
 from datetime import datetime
 from PIL import Image, ImageEnhance
-from pykakasi import kakasi
+
+from pykakasi_lite import kakasi
 
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
